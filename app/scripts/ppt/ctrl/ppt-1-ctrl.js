8f17c81360b76d3ad036c33c64a5f86874c98d8c
tianyn1990 <tianyanan.zero@gmail.com>
2014-11-28 17:12:32 +0800

    tianyn:js

--- app/scripts/ppt/ctrl/ppt-1-ctrl.js
+++ app/scripts/ppt/ctrl/ppt-1-ctrl.js
@@ -8,9 +8,46 @@ define(
 
                     var ff = $scope._f;
                     var s = $scope.s = {
+                        itv: undefined
                     };
 
+                    ff
+                        .then(function (data, defer) {
+                            ff.log("原数据：" + data);
+                            ff.alert("data乘以2", function () {
+                                defer.resolve(data * 2);
+                            });
+                        }, 1000)
+                        .then(function (data, defer) {
+                            ff.log("乘以2结果：" + data);
+                            ff.prompt("请输入：", function (e, str) {
+                                if (e) {
+                                    ff.success("你输入了：" + str);
+                                    data = parseInt(str, 10);
+                                } else {
+                                    ff.error("你取消了输入。");
+                                }
+                                var c = 5;
+                                defer.resolve(data);
+                                s.itv = $interval(function () {
+                                    if (--c === 0) {
+                                        $interval.cancel(s.itv);
+                                    } else {
+                                        ff.log(c.toString());
+                                    }
+                                }, 1000);
+                            }, data.toString());
+                        }, 1000)
+                        .then(function (data) {
+                            ff.confirm("是否继续乘以2？", function (e) {
+                                data = e ? data * 2 : data;
+                                ff.log("最终结果：" + data);
+                            });
+                        }, 5000)
+                        .go(111);
+
                     $scope.$on('$destroy', function () {
+                        $interval.cancel(s.itv);
                     });
                 }]);
 
